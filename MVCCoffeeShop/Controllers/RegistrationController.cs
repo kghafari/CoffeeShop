55c6503089d9aaed594e8442ea1ac18fd00a1d9a
kghafari <kghafari@users.noreply.github.com>
2020-02-24 16:37:00 -0500

    Initial setup. Still needs: Loopback to UserForm when server side validation isn't met. Email regex validation on model. Add some extra forms. A 'repeat password' form would be a good start.

---
+++ MVCCoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCCoffeeShop.Models;
+
+namespace MVCCoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayUser(RegisterUser user)
+        {
+            user.GetUserName();
+            return View("DisplayUser", user);
+        }
+    }
+}
---
+++ MVCCoffeeShop/Models/RegisterUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoffeeShop.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 30 characters.")]
+        public string UserName { get; set; }
+
+        [Required]
+        //[RegularExpression(@"", ErrorMessage = "Email must... be an email.")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*\d).{4}$", ErrorMessage = "Password must be at least 4 characters, and include a number")]
+        public string Password { get; set; }
+
+        public void GetUserName()
+        {
+            UserName = UserName;
+        }
+
+    }
+}
